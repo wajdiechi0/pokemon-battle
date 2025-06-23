083e33fcceb5e3a684ae03e8133c15948346e14b
Wajdi Echi <wajdi@safetyteksoftware.com>
2025-06-23 20:39:57 +0100

    circleci CI/CD config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeWrapper from "../components/ThemeWrapper";
+import { Suspense } from "react";
 
 const geist = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
         className={`${geist.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeWrapper>
-          {children}
+          <Suspense>
+            {children}
+          </Suspense>
         </ThemeWrapper>
       </body>
     </html>
